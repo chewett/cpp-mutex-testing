ffb1b3b69045920b29989ad7b706624e9d7990ee
Christopher Hewett <chewett@hotmail.co.uk>
2014-03-13 20:56:38 +0000

    Fixed dining with basic mutexes and they work

--- test_dining.cpp
+++ test_dining.cpp
@@ -6,20 +6,32 @@
 
 Chopstick chop[] = {Chopstick(0), Chopstick(1), Chopstick(2), Chopstick(3), Chopstick(4)};
 
+pthread_mutex_t sticks[5];
+
+void pickUp(int stick, int person) {
+    pthread_mutex_lock(&sticks[stick]);
+    printf("p:%i c:%i picking\n", person, stick);
+}
+
+void putDown(int stick, int person) {
+    printf("p:%i c:%i dropping\n", person, stick);
+    pthread_mutex_unlock(&sticks[stick]);
+}
+
 
 void* person(void* args) {
     int value = *(int*)(&args);
     while(true) {
         printf("p:%i picking up\n", value);
-        chop[value + 1 % 5].pickUp(value);
-        chop[value + 4 % 5].pickUp(value);
+        pickUp((value + 1) % 5, value);
+        pickUp((value + 4) % 5, value);
         printf("p:%i im eating, number\n", value);
-        sleep(1000);
-        printf("p:%i putting down\n", value);
-        chop[value + 1 % 5].putDown();
-        chop[value + 4 % 5].putDown();
+        sleep(1);
+        printf("p:%i start putting down\n", value);
+        putDown((value + 1) % 5, value);
+        putDown((value + 4) % 5, value);
         printf("p:%i thinking\n", value);
-        sleep(1500);
+        sleep(1.5);
     }
 
     pthread_exit(NULL);
@@ -29,7 +41,6 @@ int main(void) {
     std::printf("Testing working\n");
 
     pthread_t threads[5];
-    
     for(int i = 0; i<5; i++) {
         (void)pthread_create(&threads[i], NULL, person, (void*)i);
     }
