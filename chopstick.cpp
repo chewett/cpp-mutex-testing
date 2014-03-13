84bb9f09e913c6953f86fcdd6b4ab0f6a4d07c72
Christopher Hewett <chewett@hotmail.co.uk>
2014-03-13 23:43:43 +0000

    Added proper object usage of dining test

--- chopstick.cpp
+++ chopstick.cpp
@@ -7,7 +7,7 @@ Chopstick::Chopstick(int id) {
 
 void Chopstick::pickUp(int personId) {
     pthread_mutex_lock(&mut);
-    printf("p: %i c:%i picking up\n", personId, chopId);
+    //printf("p: %i c:%i picking up\n", personId, chopId);
     if(owner != -1) {
         printf("p:%i c:%i MAJOR ISSUES, ALREADY OWNED\n",personId, chopId);
     }
@@ -16,7 +16,7 @@ void Chopstick::pickUp(int personId) {
 }
 
 void Chopstick::putDown() {
-    printf("p:%i c:%i putting down\n", owner, chopId);
+   // printf("p:%i c:%i putting down\n", owner, chopId);
     owner = -1;
     pthread_mutex_unlock(&mut);
 }
--- test_dining.cpp
+++ test_dining.cpp
@@ -1,21 +1,19 @@
 #include <cstdio>
 #include <cstdlib>
-#include "pthread.h"
 #include "unistd.h"
 #include "chopstick.h"
 
 Chopstick chop[] = {Chopstick(0), Chopstick(1), Chopstick(2), Chopstick(3), Chopstick(4)};
 
-pthread_mutex_t sticks[5];
 
 void pickUp(int stick, int person) {
-    pthread_mutex_lock(&sticks[stick]);
-    printf("p:%i c:%i picking\n", person, stick);
+    chop[stick].pickUp(person);
+    printf("p:%i c:%i picked up\n", person, stick);
 }
 
 void putDown(int stick, int person) {
     printf("p:%i c:%i dropping\n", person, stick);
-    pthread_mutex_unlock(&sticks[stick]);
+    chop[stick].putDown();
 }
 
 
@@ -25,7 +23,7 @@ void* person(void* args) {
         printf("p:%i picking up\n", value);
         pickUp((value + 1) % 5, value);
         pickUp((value + 4) % 5, value);
-        printf("p:%i im eating, number\n", value);
+        printf("p:%i im eating\n", value);
         sleep(1);
         printf("p:%i start putting down\n", value);
         putDown((value + 1) % 5, value);
@@ -38,7 +36,7 @@ void* person(void* args) {
 }
 
 int main(void) {
-    std::printf("Testing working\n");
+    printf("Testing working\n");
 
     pthread_t threads[5];
     for(int i = 0; i<5; i++) {
